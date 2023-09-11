573b152c56f71c6d19a8d0ffccb472d591f7372c
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-09-11 16:45:51 -0700

    Update Replace-with-alphabet-position.js

--- Replace-with-alphabet-position.js
+++ Replace-with-alphabet-position.js
@@ -1,6 +1,6 @@
 /*
 Welcome,
-In this kata you are required to, given a string, 
+In this challenge you are required to, given a string, 
 replace every letter with its position in the alphabet.
 If anything in the text isn't a letter, 
 ignore it and don't return it.
